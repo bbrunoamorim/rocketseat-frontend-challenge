b696a0b5f060a42907bff7ba85f2ed235a5387df
Bruno Amorim <bbrunoamorim2311@gmail.com>
2023-05-26 15:13:48 -0300

    feat: render products on main page

--- rocketseat-frontend-challenge/src/app/page.tsx
+++ rocketseat-frontend-challenge/src/app/page.tsx
@@ -1,7 +1,39 @@
-export default function Home() {
+import { apiRequest } from '../../api/api'
+import Image from 'next/image'
+
+export default async function Home() {
+  const {
+    data: {
+      data: { allProducts },
+    },
+  } = await apiRequest()
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Teste Técnico Rocketseat</h1>
+    <main className="min-h-screen p-24">
+      <div className="grid grid-cols-4 gap-5">
+        {allProducts.map((product) => {
+          return (
+            <div key={product.id}>
+              <Image
+                src={product.image_url}
+                alt="Product Image"
+                width={256}
+                height={300}
+                className="rounded-t-lg h-[300px] w-[256px] object-cover"
+                priority={true}
+              />
+              <p className="text-sm font-saira text-gray-500 mt-1 ml-1 border-b-2 w-[210px]">
+                {product.name}
+              </p>
+              <p className="text-sm font-saira text-gray-700 font-semibold ml-2 mt-2">
+                {`R$ ${(product.price_in_cents / 100)
+                  .toFixed(2)
+                  .replace('.', ',')}`}
+              </p>
+            </div>
+          )
+        })}
+      </div>
     </main>
   )
 }
